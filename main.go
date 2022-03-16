8539eb4fa3abad47aa5ef748676f2b29154ad35a
marky <markvergelbanguis@gmail.com>
2022-03-16 21:55:44 +0800

    #1 - Add Sum

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+func main() {
+	fmt.Println("Leet Code Practice")
+}
+
+// func main() {
+// 	// 10
+// 	// 19
+// 	// 27
+// 	// 34
+// 	// 40
+// 	// 45
+// 	// 49
+// 	// 52
+// 	// 54
+// 	// 55
+// 	numbers := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+// 	target := 54
+// 	fmt.Println(AddSum(numbers, target))
+// }
+
+// func AddSum(nums []int, target int) []int {
+// 	currentValue := 0
+// 	startingIndex := 0
+// 	matcherIndex := 0
+// 	for index, num := range nums {
+// 		if currentValue == target {
+// 			matcherIndex = index
+// 		}
+// 		currentValue = currentValue + num
+// 	}
+// 	return []int{startingIndex, matcherIndex}
+// }
